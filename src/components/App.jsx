efe532f063413df026f4da113e3b26844212905e
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-11-18 22:08:30 -0200

    Begin fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,6 +22,8 @@ export default class App extends Component {
           ))}
         </select>
         {selectedUser && <WishListView wishList={selectedUser.wishList} />}
+        {selectedUser &&
+          <button onClick={selectedUser.getSuggestions}>Suggestions</button>}
       </div>
     )
   }
--- src/models/Group.js
+++ src/models/Group.js
@@ -1,12 +1,25 @@
 import { types } from 'mobx-state-tree'
 import { WishList } from './WishList'
 
-const User = types.model({
-  id: types.number,
-  name: types.string,
-  gender: types.enumeration('gender', ['m', 'f']),
-  wishList: types.optional(WishList, {})
-})
+const User = types
+  .model({
+    id: types.number,
+    name: types.string,
+    gender: types.enumeration('gender', ['m', 'f']),
+    wishList: types.optional(WishList, {})
+  })
+  .actions(self => ({
+    async getSuggestions () {
+      const response = await window.fetch(
+        `http://localhost:3001/suggestions_${self.gender}`
+      )
+      const suggestions = await response.json()
+      self.addSuggestions(suggestions)
+    },
+    addSuggestions (suggestions) {
+      self.wishList.items.push(...suggestions)
+    }
+  }))
 
 export const Group = types.model({
   users: types.optional(types.array(User), [])
